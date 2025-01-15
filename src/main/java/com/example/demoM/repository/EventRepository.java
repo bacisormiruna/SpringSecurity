ed7ca452fc18d318c45193b4b647d9665aacde52
bacisormiruna <mirunabacisor06@gmail.com>
2025-01-15 22:21:03 +0200

    Added final classes for creating an event

--- src/main/java/com/example/demoM/repository/EventRepository.java
+++ src/main/java/com/example/demoM/repository/EventRepository.java
@@ -1,12 +1,15 @@
 package com.example.demoM.repository;
 
 import com.example.demoM.model.Event;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
     // Metodă pentru a găsi toate evenimentele (implicit din JpaRepository, dar adăugată pentru claritate)
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = {"eventCategory"})
     List<Event> findAll();
 }
 
